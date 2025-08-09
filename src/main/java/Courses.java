48ffa67039e62b461d9dd433e773ab2fe0d7456f
dilyanabodurova <diliana.bodurova@abv.bg>
2025-08-09 15:22:45 +0300

    maps

--- src/main/java/Courses.java
+++ src/main/java/Courses.java
@@ -3,35 +3,28 @@ import java.util.*;
 public class Courses {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Map<Course, String> courses = new LinkedHashMap<>();
+        Map<String, Course> courses = new LinkedHashMap<>();
         String input = sc.nextLine();
-        while(!input.equals("end")){
+        while (!input.equals("end")) {
             List<String> data = Arrays.stream(input.split(":")).toList();
+            boolean isCoursePresent=courses.containsKey(data.get(0));
+            if(!isCoursePresent){
 
+            }else{
+                Course currentCourse = courses.get(data.get(0));
+                currentCourse.setStudents(data.get(1));
+                courses.put(data.get(0),currentCourse);
+            }
             input = sc.nextLine();
         }
 
     }
 
     public static class Course {
-        private String name;
         private List<String> students = new ArrayList<>();
-
-        public Course(String name, String student) {
-            this.name = name;
-            students.add(student);
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
 
         public void setStudents(String student) {
             students.add(student);
-        }
-
-        public String getName() {
-            return name;
         }
 
         public List<String> getStudents() {
