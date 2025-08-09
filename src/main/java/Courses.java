2f03355f432112d7d4d4afaa2e0cf0b4eac85711
dilyanabodurova <diliana.bodurova@abv.bg>
2025-08-09 20:02:48 +0300

    maps

--- src/main/java/Courses.java
+++ src/main/java/Courses.java
@@ -6,18 +6,24 @@ public class Courses {
         Map<String, Course> courses = new LinkedHashMap<>();
         String input = sc.nextLine();
         while (!input.equals("end")) {
-            List<String> data = Arrays.stream(input.split(":")).toList();
-            boolean isCoursePresent=courses.containsKey(data.get(0));
-            if(!isCoursePresent){
-
-            }else{
-                Course currentCourse = courses.get(data.get(0));
-                currentCourse.setStudents(data.get(1));
-                courses.put(data.get(0),currentCourse);
+            List<String> data = Arrays.stream(input.trim().split(":")).toList();
+            boolean isCoursePresent = courses.containsKey(data.get(0));
+            Course course;
+            if (!isCoursePresent) {
+                course = new Course();
+            } else {
+                course = courses.get(data.get(0));
             }
+            course.setStudents(data.get(1));
+            courses.put(data.get(0), course);
             input = sc.nextLine();
         }
-
+        for (Map.Entry<String, Course> entry : courses.entrySet()) {
+            System.out.println(entry.getKey().trim() + ": " + entry.getValue().getStudents().size());
+            for (int i = 0; i < entry.getValue().getStudents().size(); i++) {
+                System.out.println("--" + entry.getValue().getStudents().get(i));
+            }
+        }
     }
 
     public static class Course {
